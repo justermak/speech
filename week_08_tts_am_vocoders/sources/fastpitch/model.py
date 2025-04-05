74b251204d28199c8d15e9a8c9a735d58b56f6b2
justermak <andreiyaremenko9@gmail.com>
2025-04-05 19:50:08 +0300

    v1

--- week_08_tts_am_vocoders/sources/fastpitch/model.py
+++ week_08_tts_am_vocoders/sources/fastpitch/model.py
@@ -10,4 +10,136 @@ from sources.fastpitch.hparams import HParamsFastpitch
 from sources.fastpitch.common.transformer import FFTransformer
 
 
-<YOUR CODE HERE>
+class FastPitch(nn.Module, DeviceGetterMixin):
+    def __init__(self, hparams: HParamsFastpitch):
+        super().__init__()
+        self.hparams = hparams
+        n_symbols = len(SymbolsSet().symbols_to_id)
+
+        self.symbol_emb = nn.Embedding(n_symbols, hparams.symbols_embedding_dim)
+
+        self.encoder = FFTransformer(
+            n_layer=hparams.in_fft_n_layers,
+            n_head=hparams.in_fft_n_heads,
+            d_model=hparams.symbols_embedding_dim,
+            d_head=hparams.in_fft_d_head,
+            d_inner=4 * hparams.symbols_embedding_dim,
+            kernel_size=hparams.in_fft_conv1d_kernel_size,
+            dropout=hparams.p_in_fft_dropout,
+            dropatt=hparams.p_in_fft_dropatt,
+            dropemb=hparams.p_in_fft_dropemb
+        )
+
+        self.duration_predictor = TemporalPredictor(
+            input_size=hparams.symbols_embedding_dim,
+            filter_size=hparams.dur_predictor_filter_size,
+            kernel_size=hparams.dur_predictor_kernel_size,
+            dropout=hparams.p_dur_predictor_dropout,
+            n_layers=hparams.dur_predictor_n_layers
+        )
+
+        self.pitch_predictor = TemporalPredictor(
+            input_size=hparams.symbols_embedding_dim,
+            filter_size=hparams.pitch_predictor_filter_size,
+            kernel_size=hparams.pitch_predictor_kernel_size,
+            dropout=hparams.p_pitch_predictor_dropout,
+            n_layers=hparams.pitch_predictor_n_layers
+        )
+
+        self.pitch_emb = nn.Conv1d(1, hparams.symbols_embedding_dim, kernel_size=3, padding=1)
+
+        self.decoder = FFTransformer(
+            n_layer=hparams.out_fft_n_layers,
+            n_head=hparams.out_fft_n_heads,
+            d_model=hparams.symbols_embedding_dim,
+            d_head=hparams.out_fft_d_head,
+            d_inner=4 * hparams.symbols_embedding_dim,
+            kernel_size=hparams.out_fft_conv1d_kernel_size,
+            dropout=hparams.p_out_fft_dropout,
+            dropatt=hparams.p_out_fft_dropatt,
+            dropemb=hparams.p_out_fft_dropemb
+        )
+
+        self.proj = nn.Linear(hparams.symbols_embedding_dim, hparams.n_mel_channels, bias=True)
+
+    def get_encoder_out(self, batch: FastPitchBatch):
+        '''
+        Return: 
+        enc_out: 
+            Output of the first series of FFT blocks (before adding pitch embedding)
+            shape: (batch, len(text), symbols_embedding_dim)
+        enc_mask:
+            Boolean padding mask for the input text sequences
+            shape: (batch, len(text), 1)
+        '''
+        emb = self.symbol_emb(batch.texts)
+        enc_out, enc_mask = self.encoder(emb, lens=batch.text_lengths)
+        return enc_out, enc_mask
+
+    def forward(self, batch: FastPitchBatch, use_gt_durations=True, use_gt_pitch=True, max_duration=75):
+        '''
+        Flags `use_gt_durations` and `use_gt_pitch` should be both True during training and either True or False during inference.
+
+        Use the function `regulate_len` to duplicate phonemes according to durations before passing them to the decoder.
+        
+        Return:
+        mel_out:
+            Predicted mel-spectrograms
+            shape: (batch, time, mel_bins)
+        mel_lens:
+            Number of time frames in each of the predicted spectrograms
+            shape: (batch,)
+        log_dur_pred:
+            The predicted log-durations for each phoneme (the output of the duration predictor).
+            shape: (batch, len(text))
+        dur_pred:
+            The exponent of the predicted log-durations for each phoneme. Clamped to the range (0, max_duration) for numeric stability
+            shape: (batch, len(text))
+        pitch_pred:
+            The predicted pitch for each phoneme
+            shape: (batch, len(text))
+        '''
+        enc_out, enc_mask = self.get_encoder_out(batch)
+        log_dur_pred = self.duration_predictor(enc_out, enc_out_mask=enc_mask)
+        if use_gt_durations:
+            dur_pred = batch.durations
+        else:
+            dur_pred = torch.clamp(torch.exp(log_dur_pred), 0, max_duration)
+        if use_gt_pitch:
+            pitch_pred = batch.pitches
+        else:
+            pitch_pred = self.pitch_predictor(enc_out, enc_out_mask=enc_mask)
+        pitch_emb = self.pitch_emb(pitch_pred.unsqueeze(1)).transpose(1, 2)
+        enc_out += pitch_emb
+        enc_rep, mel_lens, reps = regulate_len(dur_pred, enc_out)
+        dec_out, dec_mask = self.decoder(enc_rep, lens=mel_lens)
+        mel_out = self.proj(dec_out)
+        return mel_out, mel_lens, dur_pred, log_dur_pred, pitch_pred
+
+    @torch.no_grad()
+    def infer(self, batch: FastPitchBatch, max_duration=75):
+        enc_out, dur_pred, pitch_pred = self.infer_encoder(batch, max_duration=max_duration)
+        mel_out, mel_lens = self.infer_decoder(enc_out, dur_pred)
+        return mel_out, mel_lens, dur_pred, pitch_pred
+
+    def infer_encoder(self, batch: FastPitchBatch, max_duration=75):
+        enc_out, enc_mask = self.get_encoder_out(batch)
+        if batch.durations is not None:
+            dur_pred = batch.durations
+        else:
+            log_dur_pred = self.duration_predictor(enc_out, enc_out_mask=enc_mask)
+            dur_pred = torch.clamp(torch.exp(log_dur_pred), 0, max_duration)
+        if batch.pitches is not None:
+            pitch_pred = batch.pitches
+        else:
+            pitch_pred = self.pitch_predictor(enc_out, enc_out_mask=enc_mask)
+        pitch_emb = self.pitch_emb(pitch_pred.unsqueeze(1)).transpose(1, 2)
+        enc_out += pitch_emb
+        return enc_out, dur_pred, pitch_pred
+
+    def infer_decoder(self, enc_out, dur_pred):
+        enc_rep, mel_lens, reps = regulate_len(dur_pred, enc_out)
+        dec_out, dec_mask = self.decoder(enc_rep, lens=mel_lens)
+        mel_out = self.proj(dec_out)
+        return mel_out, mel_lens
+    
